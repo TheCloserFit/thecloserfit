89fae41c069d6143e97d428e853c47e89005c87f
LeonardoTrapani <leonard.trapani@gmail.com>
2023-08-24 20:02:24 +0200

    feat: add logs + latest nextjs

--- lib/session.ts
+++ lib/session.ts
@@ -5,5 +5,7 @@ import { authOptions } from "@/lib/auth"
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions)
 
+  console.info("Retrieved session: ", session)
+
   return session?.user
 }
