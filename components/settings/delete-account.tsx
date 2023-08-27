9b9939f97146ce0746f37475ac5e718c4344a517
LeonardoTrapani <leonard.trapani@gmail.com>
2023-08-27 16:14:07 -0700

    fix: [BUG] delete account name button

--- components/settings/delete-account.tsx
+++ components/settings/delete-account.tsx
@@ -99,7 +99,7 @@ export function DeleteAccount({ userId }: DeleteAccountProps) {
               {loading && (
                 <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
               )}
-              Create Interview
+              Delete account
             </Button>
           </form>
         </Form>
