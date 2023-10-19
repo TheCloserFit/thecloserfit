f209c9766544ebdc2743b97cba219efa5ec71da2
LeonardoTrapani <leonard.trapani@gmail.com>
2023-10-19 08:12:38 -0700

    feat: restored old rds

---
+++ prisma/migrations/20231019150930_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `generatingFeedback` on the `Interview` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Interview" DROP COLUMN "generatingFeedback";
