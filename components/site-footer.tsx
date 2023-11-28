85c3e49a3b06bf5e79d1abf2d046b2a237444cba
LeonardoTrapani <leonard.trapani@gmail.com>
2023-11-28 11:02:07 -0800

    feat: reset migrations + added privacy to footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -7,6 +7,9 @@ import { Icons } from "@/components/icons"
 import { ModeToggle } from "@/components/mode-toggle"
 import { UserDropdown } from "@/components/user-dropdown"
 
+import PrivacyButton from "./iubenda/privacy"
+import TermsButton from "./iubenda/terms"
+
 interface SiteFooterProps extends React.HTMLAttributes<HTMLElement> {
   user?: Pick<User, "image" | "name" | "email">
 }
@@ -14,7 +17,7 @@ interface SiteFooterProps extends React.HTMLAttributes<HTMLElement> {
 export function SiteFooter({ user, className }: SiteFooterProps) {
   return (
     <footer className={cn(className)}>
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+      <div className="container flex flex-col items-center justify-between gap-2 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.logo width={30} />
           <p className="text-center text-sm leading-loose md:text-left">
@@ -40,9 +43,15 @@ export function SiteFooter({ user, className }: SiteFooterProps) {
             .
           </p>
         </div>
-        <div className="flex items-center gap-2">
-          {user && <UserDropdown user={user} />}
-          <ModeToggle />
+        <div className="flex flex-col items-center gap-2 md:flex-row">
+          <div className="flex gap-2 text-center text-sm leading-loose md:text-left">
+            <PrivacyButton />
+            <TermsButton />
+          </div>
+          <div className="flex items-center gap-2">
+            {user && <UserDropdown user={user} />}
+            <ModeToggle />
+          </div>
         </div>
       </div>
     </footer>
